3eceb692ff16c0e70ee7706b4a354ae5e9ad126e
josephkong17 <joskong@ucdavis.edu>
2019-02-21 22:47:21 -0800

    empty undo button

--- client/src/components/canvas/CanvasBoard.js
+++ client/src/components/canvas/CanvasBoard.js
@@ -9,6 +9,7 @@ class CanvasBoard extends Component {
         this.state = {color: 'red', lineWidth: 5};
         this.changeColor = this.changeColor.bind(this);
         this.changeWidth = this.changeWidth.bind(this);
+        this.undo = this.undo.bind(this);
     }
 
     changeColor(e) {
@@ -19,11 +20,15 @@ class CanvasBoard extends Component {
         this.setState({lineWidth: e})
     }
 
+    undo(e) {
+
+    }
+
     render(){
         return(
             <div style = {{ display: 'flex', flexDirection: 'row', height:'100%'}}>
                 <div >
-                    <Sidebar onChangeColor={this.changeColor} onChangeWidth={this.changeWidth}/>
+                    <Sidebar onChangeColor={this.changeColor} onChangeWidth={this.changeWidth} onUndo={this.undo}/>
                 </div>
                     <div style={{  backgroundColor: 'gray', border: 'solid 4px', flexGrow : 1}} >
                         <Canvas
