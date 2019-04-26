9d320586f470be3a20bb2509ae6f3ac09d124478
josephkong17 <joskong@ucdavis.edu>
2019-04-26 10:15:06 -0700

    add black and red color

--- client/src/components/layout/ColorPicker.js
+++ client/src/components/layout/ColorPicker.js
@@ -19,6 +19,9 @@ class MyColorPicker extends React.Component {
     return (
         <CirclePicker
             onChangeComplete={ this.handleChangeComplete }
+            colors={
+                ["#ff0000", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#000000", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#ff5722", "#795548", "#607d8b"]
+            }
         />
     )
   }
