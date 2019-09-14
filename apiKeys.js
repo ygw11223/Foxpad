63697cf2adb6573b54a9dea69d5ed4f7c57f8fe6
Alice Ma <aaama@ucdavis.edu>
2019-09-14 10:54:13 -0500

    Change and remove api key

---
+++ apiKeys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyDRxzREGUDWb4WDO47NonhCMIin7fzuvZA",
+    authDomain: "collaborative-drawing-d69f1.firebaseapp.com",
+    databaseURL: "https://collaborative-drawing-d69f1.firebaseio.com",
+    projectId: "collaborative-drawing-d69f1",
+    storageBucket: "collaborative-drawing-d69f1.appspot.com",
+    messagingSenderId: "957430309885"
+}
