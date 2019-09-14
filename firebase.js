79685689a4ce750dd3fca2f57752d4ebf99ff287
Alice Ma <aaama@ucdavis.edu>
2019-09-14 11:24:22 -0500

    update API keys and gitignore

--- firebase.js
+++ firebase.js
@@ -1,24 +1,15 @@
-// import * as firebase from 'firebase';
+import apiConfig from 'apiKeys';
 const firebase = require('firebase');
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyB14NusDLxx5iPcdIfSdiZBN4HhiF6je1s",
-  authDomain: "collaborative-drawing-d69f1.firebaseapp.com",
-  databaseURL: "https://collaborative-drawing-d69f1.firebaseio.com",
-  projectId: "collaborative-drawing-d69f1",
-  storageBucket: "collaborative-drawing-d69f1.appspot.com",
-  messagingSenderId: "957430309885"
+  apiKey: apiConfig.apiKey,
+  authDomain: apiConfig.authDomain,
+  databaseURL: apiConfig.databaseURL,
+  projectId: apiConfig.projectId,
+  storageBucket: apiConfig.storageBucket,
+  messagingSenderId: apiConfig.messagingSenderId
 };
 firebase.initializeApp(config);
-
-// var admin = require("firebase-admin");
-//
-// var serviceAccount = require("collaborative-drawing-d69f1-firebase-adminsdk-02pbp-562c33e034.json");
-//
-// admin.initializeApp({
-// credential: admin.credential.cert(serviceAccount),
-// databaseURL: "https://collaborative-drawing-d69f1.firebaseio.com"
-// });
 
 module.exports = firebase;
